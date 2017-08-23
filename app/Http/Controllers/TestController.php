47e6b2973e4bccab42225b16ae9fcd0222c41c38
brandonglennt <brandonglennt@gmail.com>
2017-08-23 15:56:23 -0400

    Created API Routes

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Response;
+use Purifier;
+
+
+class TestController extends Controller
+{
+  public function index ()
+  {
+    $data = "here is my data";
+
+    return Response::json(['data' => $data]);
+  }
+
+  public function store(Request $request)
+  {
+    $rules = [
+      'name' => 'required',
+      'age' => 'required'
+    ];
+
+    $validator = Validator::make(Purifier::clean($request->all()), $rules);
+
+    if($validator->fails())
+    {
+      return Response::json(['error' => 'Please fill out all fields.']);
+    }
+
+    $name = $request->input('name');
+    $age = $request->input('age');
+    $home = $request->input('home');
+
+    if($validator->fails())
+    {
+      if($name == NULL)
+      {
+        return Response::json(['error' => 'Please fill out your name']);
+      }
+      elseif($age == NULL) {
+        return Response::json(['error' => 'Please enter you age.']);
+      }
+    }
+
+    $sentence = "Hi! My name is " . $name . " and I am " . $age . "years old. I live in " . $home . ".";
+
+    return Response::json(['sentence' => $sentence]);
+  }
+}
